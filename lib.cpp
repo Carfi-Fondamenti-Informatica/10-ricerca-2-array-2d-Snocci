dd801201fd64dce25b097eafdfdd850f3b9a1f9f
Snocci <128066678+Snocci@users.noreply.github.com>
2023-06-08 22:58:25 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -13,7 +13,15 @@ void init(char mat[5][15]){
     }
 }
 void inserimento (char mat[], int n) {
-    cin>> mat;
+    char x[n*10];
+    init(x,n*10);
+    for (int i=0;i<(n*10);i++){
+        mat[i]=x[n*10];
+    }
+    cin>> x;
+    for(int i=0;i<n;i++){
+        mat[i]=x[i];
+    }
 }
 void inserimento(char nomi[5][15], char cognomi[5][15]){
     for(int i=0; i<5;i++){
@@ -24,9 +32,15 @@ void inserimento(char nomi[5][15], char cognomi[5][15]){
 
 int ricerca(char nomi[][15],char cognomi[][15],char nome[],char cognome[] ) {
     for (int i=0; i<5; i++){
-        if (nomi[i][15]==nome[15] && cognomi[i][15]==cognome[15]) {
-            return i;
+        int j=0;
+        for (j=0;j<15;j++){
+            if(cognomi[i][j]!=cognome[i]){
+                break;}
+            if(nomi[i][j]!=nome[i]){
+                break;}
+            if (j==15){
+                return i;}
         }
-    } return -1;
+        return -1;
 }
 
